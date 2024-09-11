7d15d22130bced3fcc12f25c4271353d9cfe3902
Fernando <faguirrelaborde@gmail.com>
2024-09-10 20:59:57 -0600

    Updated CSS and pop-up functionality

--- src/components/PaymentPopup.js
+++ src/components/PaymentPopup.js
@@ -1,58 +1,59 @@
 import React from 'react';
-import checkoutLogo from './assets/checkout.png';
-import nuveiLogo from './assets/nuvei.png';
-import skrillLogo from './assets/skrill.png';
-import netellerLogo from './assets/neteller.png';
-import mastercardIcon from './assets/mastercard.png';
-import visaIcon from './assets/visa.png';
-import applePayIcon from './assets/apple-pay.png';
-import googlePayIcon from './assets/google-pay.png';
+import checkoutLogo from '../assets/checkout.png';
+import nuveiLogo from '../assets/nuvei.png';
+import skrillLogo from '../assets/skrill.png';
+import netellerLogo from '../assets/neteller.png';
+import mastercardLogo from '../assets/mastercard.png';
+import visaLogo from '../assets/visa.png';
+import applePayLogo from '../assets/apple-pay.png';
+import googlePayLogo from '../assets/google-pay.png';
 
 const PaymentPopup = () => {
   return (
     <div className="popup-overlay">
       <div className="popup-container">
-        <h2>Seguro de Cuenta Aceptado</h2>
-        <p>
-          Estimado Trader, su solicitud de seguro ha sido aceptada y está pendiente de la realización del pago. A continuación, seleccione su método de pago para completar el proceso.
-        </p>
+        <h2>ESTIMADO TRADER</h2>
+        <p>SU SOLICITUD DE SEGURO HA SIDO ACEPTADA Y ESTÁ PENDIENTE DE LA REALIZACIÓN DEL PAGO. A CONTINUACIÓN, SELECCIONE SU MÉTODO DE PAGO PARA COMPLETAR EL PROCESO.</p>
         <div className="payment-methods">
-          {/* Primer método de pago */}
+          {/* First method - Checkout */}
           <div className="method">
             <img src={checkoutLogo} alt="Checkout Logo" className="payment-logo" />
-            <hr className="payment-divider" />
+            <div className="payment-divider"></div>
             <div className="payment-icons">
-              <img src={mastercardIcon} alt="Mastercard Icon" />
-              <img src={visaIcon} alt="Visa Icon" />
+              <img src={mastercardLogo} alt="Mastercard" />
+              <img src={visaLogo} alt="Visa" />
             </div>
           </div>
-          {/* Segundo método de pago */}
+
+          {/* Second method - Nuvei */}
           <div className="method">
             <img src={nuveiLogo} alt="Nuvei Logo" className="payment-logo" />
-            <hr className="payment-divider" />
+            <div className="payment-divider"></div>
             <div className="payment-icons">
-              <img src={mastercardIcon} alt="Mastercard Icon" />
-              <img src={applePayIcon} alt="Apple Pay Icon" />
-              <img src={googlePayIcon} alt="Google Pay Icon" />
-              <img src={visaIcon} alt="Visa Icon" />
+              <img src={mastercardLogo} alt="Mastercard" />
+              <img src={applePayLogo} alt="Apple Pay" />
+              <img src={googlePayLogo} alt="Google Pay" />
+              <img src={visaLogo} alt="Visa" />
             </div>
           </div>
-          {/* Tercer método de pago */}
+
+          {/* Third method - Skrill */}
           <div className="method">
             <img src={skrillLogo} alt="Skrill Logo" className="payment-logo" />
-            <hr className="payment-divider" />
+            <div className="payment-divider"></div>
             <div className="payment-icons">
-              <img src={mastercardIcon} alt="Mastercard Icon" />
-              <img src={visaIcon} alt="Visa Icon" />
+              <img src={mastercardLogo} alt="Mastercard" />
+              <img src={visaLogo} alt="Visa" />
             </div>
           </div>
-          {/* Cuarto método de pago */}
+
+          {/* Fourth method - Neteller */}
           <div className="method">
             <img src={netellerLogo} alt="Neteller Logo" className="payment-logo" />
-            <hr className="payment-divider" />
+            <div className="payment-divider"></div>
             <div className="payment-icons">
-              <img src={mastercardIcon} alt="Mastercard Icon" />
-              <img src={visaIcon} alt="Visa Icon" />
+              <img src={mastercardLogo} alt="Mastercard" />
+              <img src={visaLogo} alt="Visa" />
             </div>
           </div>
         </div>
